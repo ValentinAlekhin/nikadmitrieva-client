fea0d6df22b71d3f5ffd03e2e85fb1ccfeec2d95
ValentinAlekhin <headbangernetwork@gmail.com>
2020-04-05 18:29:35 +0300

    Create contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import classes from './Contact.module.scss'
 
 export default class Contact extends Component {
   render() {
     return (
-      <h1>Contact</h1>
+      <div className={classes.Contact}>
+        <a href="mailto:nikadmitrieva.v@gmail.com" type="email">nikadmitrieva.v@gmail.com</a>
+      </div>
     )
   }
 }

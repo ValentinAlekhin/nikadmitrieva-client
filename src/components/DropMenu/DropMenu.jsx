2a77245cf31552d77847b176b2f7573b87d2bcbc
ValentinAlekhin <headbangernetwork@gmail.com>
2020-05-11 17:27:11 +0300

    Add placeholder to GalleryCard & animation in DropMenu

--- src/components/DropMenu/DropMenu.jsx
+++ src/components/DropMenu/DropMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './DropMenu.module.scss'
 import { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 export default ({ 
   title, subTitles
@@ -17,29 +18,37 @@ export default ({
     setIsOpen(true)
   }
 
-  const cls = [classes.DropContainer]
-  if (isOpen) {
-    cls.push(classes.Show)
-  } else {
-    cls.push(classes.Close)
-  }
+  const classNames={
+    enter: classes.enter,
+    enterActive: classes['enter-active'],
+    exit: classes.exit,
+    exitActive: classes['exit-active'],
+   }
 
   return (
     <div className={classes.DropMenu} onMouseLeave={hideDropMenu}>
       <span className={classes.DropTitle} onClick={showDropMenu} onMouseEnter={showDropMenu}>{ title }</span>
-      { isOpen && <ul className={cls.join(' ')}>
-          { subTitles.map(({route, title}, index) => (
-            <li 
-              className={classes.DropItem} 
-              key={index}
-              onClick={hideDropMenu}
-            >
-              <NavLink className={classes.DropLink} to={route}>
-                { title }
-              </NavLink>
-            </li>
-          )) }
-        </ul> }
+      <CSSTransition
+        in={isOpen}
+        timeout={200}
+        classNames={classNames}
+        unmountOnExit
+        mountOnEnter 
+      >
+        <ul className={classes.DropContainer}>
+            { subTitles.map(({route, title}, index) => (
+              <li 
+                className={classes.DropItem} 
+                key={index}
+                onClick={hideDropMenu}
+              >
+                <NavLink className={classes.DropLink} to={route}>
+                  { title }
+                </NavLink>
+              </li>
+            )) }
+          </ul>
+        </CSSTransition>
     </div>
   )
 }
--- src/components/GalleryCard/GalleryCard.jsx
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -4,13 +4,46 @@ import classes from './GalleryCard.module.scss'
 import { connect } from 'react-redux'
 import { useState } from 'react'
 import ImgControlPanel from '../../UI/ImgControlPanel/ImgControlPanel'
+import { CSSTransition } from 'react-transition-group'
 
 const GalleryCard = ({
-  img, link, title, 
+  img: { main, placeholder }, 
+  link, title, 
   isLogin, id
 }) => {
 
   const [hover, setHover] = useState(false)
+  const [imgLoad, setImgLoad] = useState(false)
+
+  const classNames={
+    enter: classes.enter,
+    enterActive: classes['enter-active'],
+    exit: classes.exit,
+    exitActive: classes['exit-active'],
+   }
+
+  const renderPicture = () => {
+    const webpSrc = []
+    const jpegSrc = []
+
+    Object.entries(main).forEach(([tag, { webp, jpeg }]) => {
+      webpSrc.push(`${webp} ${tag}`)
+      jpegSrc.push(`${jpeg} ${tag}`)
+    })
+
+    return (
+      <picture>
+        <source srcSet={webpSrc.join(', ')} type="image/webp"/>
+        <source srcSet={jpegSrc.join(', ')} type="image/jpeg"/>
+        <img 
+          src={main['800w'].jpeg} 
+          className={classes.ImgItem} 
+          alt={title}
+          onLoad={() => setImgLoad(true)}
+          />
+      </picture>
+    )
+  }
 
   return (
     <div 
@@ -26,11 +59,20 @@ const GalleryCard = ({
         className={classes.ImgContainer} 
         to={link} 
         >
-        <picture className={classes.ImgItem}>
-          <source srcSet={img.webp} type="image/webp"/>
-          <source srcSet={img.jpg} type="image/jpg"/>
-          <img className={classes.ImgItem} src={img.jpg} alt={title}/>
-        </picture>
+        <CSSTransition
+          in={!imgLoad}
+          timeout={300}
+          classNames={classNames}
+          unmountOnExit
+          mountOnEnter
+        >
+          <img 
+            srcSet={placeholder} 
+            className={classes.ImgPlaceholder} 
+            alt={title}
+          />
+        </CSSTransition>
+        { renderPicture() }
       </NavLink>
       <NavLink className={classes.GalerryTitle} to={link}>
         <h4>
